4152134268199cdc40b432662082afeccc716820
Raman Andreyuk <romaandreyuk@gmail.com>
2025-11-13 21:20:20 +0100

    work with navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,33 +1,52 @@
-import { Link } from "react-router-dom"
+import {Link} from "react-router-dom"
 import styles from "./Navbar.module.scss"
+import {Bell, Bookmark, Flag, Save, User, UserIcon} from "lucide-react";
 
 
 export default function Navbar() {
     return (
         <nav className={styles.navbar}>
             <div className={styles.left}>
-<div className={styles.logo}>
-    <span className={styles.bold}>NO</span> FLUFF <br /> JOBS
-</div>
-            <Link to="/jobs">OFERTY PRACY</Link>
-            <Link to="/companies">PROFILE FIRM</Link>
-            <Link to="/calculator" className={styles.new}>KALKULATOR WYNAGRODZEN <span className={styles.badge}>NEW</span></Link>
+                <div className={styles.logo}>
+                    <Link to="/" className={styles.logoLink}> <span className={styles.bold}>NO</span>
+                        <span>FLUFF</span>
+                        <span>JOBS</span> </Link>
+                </div>
+                <Link to="/jobs">OFERTY PRACY</Link>
+                <Link to="/companies">PROFILE FIRM</Link>
+                <Link to="/calculator" className={styles.new}>KALKULATOR WYNAGRODZEN <span
+                    className={styles.badge}>NEW</span></Link>
             </div>
-<div className={styles.right}>
-    <Link to="/employers">DLA PRACODAWCY</Link>
-    <Link to="/pricing">CENNIK</Link>
-    <Link to="/publish">PUBLIKUJ</Link>
-    <Link to="/login">ROMAANDREYUK</Link>
+            <div className={styles.right}>
+                <Link to="/employers">DLA PRACODAWCY</Link>
+                <Link to="/pricing">CENNIK</Link>
+                <Link to="/publish">PUBLIKUJ</Link>
+                <Link to="/login" className={styles.userLink}>
+                    <User className={styles.userIcon}/>
+                    <span>ROMAANDREYUK</span>
+                </Link>
 
 
+                <div className={styles.icons}>
 
-    <div className={styles.icons}>
-        <i className="fa-regular fa-bookmark"></i>
-        <i className="fa-regular fa-bell"></i>
-        <div className={styles.flag}></div>
-    </div>
+                    <Link to="/saved" className={styles.iconLink}>
+                    <div className={styles.iconWrapper}>
+                        <Bookmark className={styles.icon}/>
+                        <span className={styles.notifBadge}>2</span>
+                    </div>
+                    </Link>
 
-</div>
+                    <Link to="/notifications" className={styles.iconLink}>
+                        <Bell className={styles.icon} />
+                    </Link>
+
+                    <Link to="/language" className={styles.iconLink}>
+                        <div className={styles.flag}></div>
+                    </Link>
+                </div>
+
+            </div>
         </nav>
     )
 }
+
