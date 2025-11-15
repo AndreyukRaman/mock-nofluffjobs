a70866f2f2b05dc2976d9363e9bc2f81c1cd2634
Dima Andreyuk <dm.andreyuk@gmail.com>
2025-11-15 16:39:38 +0100

    fix import scss modules

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+// https://stackoverflow.com/a/71581759
+
+// We need to tell TypeScript that when we write "import styles from './styles.scss' we mean to load a module (to look for a './styles.scss.d.ts').
+declare module "*.scss";
